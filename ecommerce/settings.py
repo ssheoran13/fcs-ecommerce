02c0be229131ee4485d58403a05c0871d80a6782
samikprakash <samikprakash@gmail.com>
2021-11-10 23:36:51 +0530

    final

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -2,7 +2,7 @@ import os
 
 ENVIRONMENT = os.getenv('ENVIRONMENT', 'development')
 
-DEBUG = True
+DEBUG = False
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-05sgp9!deq=q1nltm@^^2cc+v29i(tyybv3v2t77qi66czazj'
 ALLOWED_HOSTS = ['192.168.3.38','127.0.0.1']
